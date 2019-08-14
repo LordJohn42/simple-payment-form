d7408b6095a5df2054faad91dd3dea3bbe4b1630
Eugene <eugene.li.eu@gmail.com>
2019-08-14 21:30:53 +0700

    Add env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl : 'https://api.paymentwall.com',
+  apiKey: '6f48a1084b8dc4bce0a3e580e4ae49b7',
+  sign: '0a9fa532a8df8ceaa5bd1071c5393fcf',
+  countryApi: 'https://restcountries.eu/rest/v2/alpha/'
 };
